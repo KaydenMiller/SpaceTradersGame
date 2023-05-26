f629b7963f9ac1b112915092e5ab7abff0e4e0b7
Kayden Miller <kaydenmiller1@gmail.com>
2023-05-25 23:27:58 -0600

    feat: add remainder of contract properties

--- SpaceTraders/Pages/Contracts/Contract.cs
+++ SpaceTraders/Pages/Contracts/Contract.cs
@@ -13,6 +13,21 @@ public class Contract
     [JsonPropertyName("type")]
     public string Type { get; set; } = default!;
 
+    [JsonPropertyName("accepted")]
+    public bool Accepted { get; set; }
+
+    [JsonPropertyName("fulfilled")]
+    public bool Fulfilled { get; set; }
+
+    [JsonPropertyName("expiration")]
+    public DateTime Expiration { get; set; }
+
+    [JsonPropertyName("deadlineToAccept")]
+    public DateTime DeadlineToAccept { get; set; }
+
+    [JsonPropertyName("terms")]
+    public ContractTerms Terms { get; set; } = default!;
+
 
     public class ContractTerms
     {
@@ -20,7 +35,10 @@ public class Contract
         public DateTime Deadline { get; set; }
 
         [JsonPropertyName("payment")]
-        public ContractPayment Payment { get; set; }
+        public ContractPayment Payment { get; set; } = default!;
+
+        [JsonPropertyName("deliver")] 
+        public IEnumerable<Delivery>? Deliveries { get; set; } 
         
         public class ContractPayment
         {
@@ -29,6 +47,21 @@ public class Contract
 
             [JsonPropertyName("onFulfilled")]
             public int OnFulfilled { get; set; }
+        }
+
+        public class Delivery
+        {
+            [JsonPropertyName("tradeSymbol")]
+            public string TradeSymbol { get; set; }
+
+            [JsonPropertyName("destinationSymbol")]
+            public string DestinationSymbol { get; set; }
+
+            [JsonPropertyName("unitsRequired")]
+            public int UnitsRequired { get; set; }
+
+            [JsonPropertyName("unitsFulfilled")]
+            public int UnitsFulfilled { get; set; }
         }
     }
 }
