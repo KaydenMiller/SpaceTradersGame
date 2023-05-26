486ec1e12af2c949d80d940d22c3ec8e13f67226
Kayden Miller <kaydenmiller1@gmail.com>
2023-05-25 21:38:32 -0600

    feat: add secrets management

--- SpaceTraders/Pages/Contracts/ContractApiService.cs
+++ SpaceTraders/Pages/Contracts/ContractApiService.cs
@@ -6,13 +6,20 @@ namespace SpaceTraders.Pages.Contracts;
 public class ContractApiService
 {
     public const string API = "https://api.spacetraders.io/v2";
-    public const string TOKEN = "";
+
+    private string _token;
+
+    public ContractApiService(IConfiguration configuration)
+    {
+        _token = configuration["SpaceTraders:ApiKey"]!;
+    }
+    
     public async Task<IEnumerable<Contract>> GetContracts()
     {
         var response = await API
            .AppendPathSegment("my")
            .AppendPathSegment("contracts")
-           .WithOAuthBearerToken(TOKEN)
+           .WithOAuthBearerToken(_token)
            .GetJsonAsync<SpaceTradersResponse<Contract>>();
 
         return response.Data;
