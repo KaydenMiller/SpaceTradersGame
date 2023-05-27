b0a64f6aba2e467ee6661e98c0f8107722c8964c
Kayden Miller <kaydenmiller1@gmail.com>
2023-05-26 23:25:54 -0600

    feat: add the ability to accept contracts

---
+++ SpaceTraders/Pages/Contracts/AcceptContractResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SpaceTraders.Pages.Contracts;
+
+public class AcceptContractResponse
+{
+    [JsonPropertyName("agent")]
+    public Player.Player Player { get; set; }
+
+    [JsonPropertyName("contract")]
+    public Contract Contract { get; set; }
+}
