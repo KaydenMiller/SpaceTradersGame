649534c92428fe82f68ae0b7ff13ba6c7ce7cac4
Kayden Miller <kaydenmiller1@gmail.com>
2023-05-26 23:29:32 -0600

    chore: remove weather data service

--- SpaceTraders/Program.cs
+++ SpaceTraders/Program.cs
@@ -1,4 +1,3 @@
-using SpaceTraders.Data;
 using SpaceTraders.Pages.Contracts;
 using SpaceTraders.Pages.Player;
 
@@ -7,7 +6,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddSingleton<ContractApiService>();
 builder.Services.AddSingleton<PlayerApiService>();
 
