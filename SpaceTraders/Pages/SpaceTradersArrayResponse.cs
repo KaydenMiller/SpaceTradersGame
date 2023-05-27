b3f5a3a0e7ac73973118a9e24666559487b473ba
Kayden Miller <kaydenmiller1@gmail.com>
2023-05-26 21:41:24 -0600

    feat: add meta data for response

--- SpaceTraders/Pages/SpaceTradersArrayResponse.cs
+++ SpaceTraders/Pages/SpaceTradersArrayResponse.cs
@@ -7,6 +7,21 @@ public class SpaceTradersArrayResponse<TData>
 {
     [JsonPropertyName("data")] 
     public IEnumerable<TData> Data { get; set; } = default!;
+
+    [JsonPropertyName("meta")] 
+    public Metadata Meta { get; set; } = default!;
+
+    public class Metadata
+    {
+        [JsonPropertyName("total")]
+        public int Total { get; set; }
+        
+        [JsonPropertyName("page")]
+        public int Page { get; set; }
+       
+        [JsonPropertyName("limit")]
+        public int Limit { get; set; }
+    }
 }
 
 public class SpaceTradersObjectResponse<TData> where TData : class
