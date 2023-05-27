6c9945d2e31ec100863c3c9a8d0842caf6139e0b
Kayden Miller <kaydenmiller1@gmail.com>
2023-05-27 00:01:46 -0600

    chore: update to use mudblazor

--- SpaceTraders/Program.cs
+++ SpaceTraders/Program.cs
@@ -1,11 +1,12 @@
+using MudBlazor.Services;
 using SpaceTraders.Pages.Contracts;
 using SpaceTraders.Pages.Player;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddMudServices();
 builder.Services.AddSingleton<ContractApiService>();
 builder.Services.AddSingleton<PlayerApiService>();
 
@@ -15,7 +16,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
