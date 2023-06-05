eb97b004a1380e5a511e6f239fd8a051ee892161
Kayden Miller <kaydenmiller1@gmail.com>
2023-06-04 23:15:39 -0600

    chore: add better logging and support seq

--- SpaceTraders/Program.cs
+++ SpaceTraders/Program.cs
@@ -4,6 +4,7 @@ using Flurl.Http;
 using Flurl.Http.Configuration;
 using MudBlazor;
 using MudBlazor.Services;
+using Serilog;
 using SpaceTraders.Pages;
 using SpaceTraders.Pages.Contracts;
 using SpaceTraders.Pages.Location;
@@ -13,82 +14,107 @@ using SpaceTraders.Pages.Ship;
 using SpaceTraders.Pages.ShipScripts;
 using SpaceTraders.Pages.ShipYard;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+   .Enrich.FromLogContext()
+   .WriteTo.Console()
+   .WriteTo.File(
+        path: ".",
+        rollingInterval: RollingInterval.Hour,
+        fileSizeLimitBytes: 10 * 1024 * 1024,
+        retainedFileCountLimit: 2,
+        rollOnFileSizeLimit: true,
+        shared: true,
+        flushToDiskInterval: TimeSpan.FromSeconds(1)
+    )
+   .WriteTo.Seq("http://localhost:5341")
+   .CreateLogger();
 
-FlurlHttp.Configure(settings =>
+try
 {
-    var options = new JsonSerializerOptions(JsonSerializerOptions.Default);
-    options.Converters.Add(new JsonStringEnumConverter());
-    options.Converters.Add(new LocationJsonConverter());
-    options.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
-    settings.JsonSerializer = new DefaultJsonSerializer(options);
+    var builder = WebApplication.CreateBuilder(args);
 
-    settings.AfterCallAsync = async call =>
+    builder.Host.UseSerilog();
+
+    FlurlHttp.Configure(settings =>
     {
-        Console.WriteLine($"REQUEST: {call.Request.Verb.Method}: {call.Request.Url}");
-        Console.WriteLine(call.RequestBody);
-        Console.WriteLine($"RESPONSE: {call.Response.StatusCode} {await call.Response.ResponseMessage.Content.ReadAsStringAsync()}");
-        foreach (var header in call.Response.Headers)
+        var options = new JsonSerializerOptions(JsonSerializerOptions.Default);
+        options.Converters.Add(new JsonStringEnumConverter());
+        options.Converters.Add(new LocationJsonConverter());
+        options.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
+        settings.JsonSerializer = new DefaultJsonSerializer(options);
+
+        settings.AfterCallAsync = async call =>
         {
-            Console.WriteLine($"\t{header.Name}: {header.Value}");
-        }
-    };
-});
+            Log.Information("HTTP {Verb} {Url} responded {StatusCode} with body {Body} containing headers {Headers}", call.Request.Verb.Method, call.Request.Url, call.Response.StatusCode, await call.Response.ResponseMessage.Content.ReadAsStringAsync(), call.Response.Headers.Select(x => new
+            {
+                Name = x.Name,
+                Value = x.Value
+            }));
+        };
+    });
 
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddMudServices(cfg =>
-{
-    cfg.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomLeft;
-    cfg.SnackbarConfiguration.PreventDuplicates = false;
-    cfg.SnackbarConfiguration.ShowCloseIcon = true;
-});;
+    builder.Services.AddRazorPages();
+    builder.Services.AddServerSideBlazor();
+    builder.Services.AddMudServices(cfg =>
+    {
+        cfg.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomLeft;
+        cfg.SnackbarConfiguration.PreventDuplicates = false;
+        cfg.SnackbarConfiguration.ShowCloseIcon = true;
+    });
 
-builder.Services.ConfigureHttpJsonOptions(options =>
-{
-    options.SerializerOptions.Converters.Add(new JsonStringEnumConverter());
-    options.SerializerOptions.Converters.Add(new LocationJsonConverter());
-    options.SerializerOptions.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
-});
+    builder.Services.ConfigureHttpJsonOptions(options =>
+    {
+        options.SerializerOptions.Converters.Add(new JsonStringEnumConverter());
+        options.SerializerOptions.Converters.Add(new LocationJsonConverter());
+        options.SerializerOptions.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
+    });
 
-builder.Services.AddSingleton<LoggerFactory>();
-builder.Services.AddSingleton<SpaceTradersApi>();
-builder.Services.AddSingleton<ContractApiService>();
-builder.Services.AddSingleton<PlayerApiService>();
-builder.Services.AddSingleton<LocationApiService>();
-builder.Services.AddSingleton<ShipYardApiService>();
-builder.Services.AddSingleton<ShipApiService>();
-builder.Services.AddSingleton<MarketApiService>();
+    builder.Services.AddSingleton<LoggerFactory>();
+    builder.Services.AddSingleton<SpaceTradersApi>();
+    builder.Services.AddSingleton<ContractApiService>();
+    builder.Services.AddSingleton<PlayerApiService>();
+    builder.Services.AddSingleton<LocationApiService>();
+    builder.Services.AddSingleton<ShipYardApiService>();
+    builder.Services.AddSingleton<ShipApiService>();
+    builder.Services.AddSingleton<MarketApiService>();
 
-builder.Services.AddSingleton<ScriptFactory>();
-builder.Services.AddSingleton<ShipScriptService>();
+    builder.Services.AddSingleton<ScriptFactory>();
+    builder.Services.AddSingleton<ShipScriptService>();
 
-builder.Services.AddTransient<Idle>();
-builder.Services.AddTransient<MineAndSell>();
-builder.Services.AddTransient<SurveyBelt>();
-builder.Services.AddTransient<AdvancedMineAndSell>();
+    builder.Services.AddTransient<Idle>();
+    builder.Services.AddTransient<MineAndSell>();
+    builder.Services.AddTransient<SurveyBelt>();
+    builder.Services.AddTransient<AdvancedMineAndSell>();
 
-builder.Services.AddMediatR(cfg =>
-{
-    cfg.RegisterServicesFromAssemblyContaining<Program>();
-});
+    builder.Services.AddMediatR(cfg => { cfg.RegisterServicesFromAssemblyContaining<Program>(); });
 
-var app = builder.Build();
+    var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    app.UseHsts();
+    app.UseSerilogRequestLogging();
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Error");
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.MapBlazorHub();
+    app.MapFallbackToPage("/_Host");
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly!");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
