ca6eb52b2a62a435cc76816241fd70ddaa2f08ff
Tanner Smith <smithjtanner@gmail.com>
2023-06-04 23:28:53 -0600

    feat: added market and shipyard details to the location details page

--- SpaceTraders/Pages/ShipYard/ShipYard.cs
+++ SpaceTraders/Pages/ShipYard/ShipYard.cs
@@ -11,11 +11,50 @@ public class ShipYard
     public Core.Location Symbol { get; set; }
 
     [JsonPropertyName("shipTypes")]
-    public IEnumerable<ShipType> Ships { get; set; }
+    public IEnumerable<ShipType> ShipTypes { get; set; }
+    
+    [JsonPropertyName("transactions")]
+    public IEnumerable<ShipTransaction> Transactions { get; set; }
+
+    [JsonPropertyName("ships")]
+    public IEnumerable<Ship> Ships { get; set; }
 }
 
 public class ShipType
 {
     [JsonPropertyName("type")]
     public ShipTypes Type { get; set; }
+}
+
+public class ShipTransaction
+{
+    [JsonPropertyName("waypoinSymbol")]
+    public string WaypointSymbol { get; set; }
+    
+    [JsonPropertyName("shipSymbol")]
+    public string ShipSymbol { get; set; }
+    
+    [JsonPropertyName("price")]
+    public int Price { get; set; }
+    
+    [JsonPropertyName("agentSymbol")]
+    public string AgentSymbol { get; set; }
+    
+    [JsonPropertyName("timestamp")]
+    public DateTime Timestamp { get; set; }
+}
+
+public class Ship
+{
+    [JsonPropertyName("type")]
+    public ShipTypes Type { get; set; }
+    
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("description")]
+    public string Description { get; set; }
+
+    [JsonPropertyName("purchasePrice")]
+    public int PurchasePrice { get; set; }
 }
